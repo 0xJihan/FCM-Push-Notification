17e85eec0a6e46b9533b86907256fdea376d9232
0xJihan <jihankhan966@gmail.com>
2025-05-02 10:23:24 +0600

    feat: Update `Constants` with OneSignal credentials
    This commit updates the `Constants` file to include necessary credentials for OneSignal integration:
    
    -   **`APP_ID`:** Added a constant for the OneSignal application ID.
    -   **`API_KEY`:** Added a constant for the OneSignal API key.

--- app/src/main/java/com/jihan/app/domain/utils/Constants.kt
+++ app/src/main/java/com/jihan/app/domain/utils/Constants.kt
@@ -1,6 +1,13 @@
 package com.jihan.app.domain.utils
 
+
+
+
 object Constants {
-    const val BASE_URL = "https://jsonplaceholder.typicode.com/"
-    const val TAG = "Jihan Khan"
+
+    const val APP_ID = "YOUR_APP_ID"
+    const val API_KEY = "YOUR_API_KEY"
+
 }
+
+
