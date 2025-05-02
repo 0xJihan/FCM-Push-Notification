21aeb193c9704fcbed85cf5438408a2c1f8bad5a
0xJihan <jihankhan966@gmail.com>
2025-05-02 10:10:25 +0600

    feat: Add FCM token display and image notification sending
    This commit introduces the ability to display the Firebase Cloud Messaging (FCM) token for the device and send image notifications. Key changes include:
    
    -   **FCM Token Display:**
        -   The FCM token is retrieved and displayed on the `HomeScreen`.
        -   Added copy button to copy FCM token to clipboard.
    -   **UI Updates:**
        -   UI is updated to be able to put the token, title, body for notifications.
    -   **Image Notification Sending:**
        -   Added a function to send image notifications via FCM.
        -   A button is added to trigger sending image notifications with sample data.
    - **Firebase token:** Add display for firebase token.
    - **LaunchedEffect:** Add `LaunchedEffect` for get the FCM token.
    - **Scaffold:** Use `Scaffold` for the main screen.

--- app/src/main/java/com/jihan/app/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/jihan/app/presentation/screens/HomeScreen.kt
@@ -1,45 +1,122 @@
 package com.jihan.app.presentation.screens
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.jihan.app.R
+import com.composables.icons.lucide.Copy
+import com.composables.icons.lucide.Lucide
+import com.google.firebase.Firebase
+import com.google.firebase.messaging.messaging
 import com.jihan.app.Routes
+import com.jihan.app.domain.utils.fcm.sendImageNotification
+import com.jihan.composeutils.CenterBox
+import com.jihan.composeutils.CxButton
+import com.jihan.composeutils.CxEditText
+import com.jihan.composeutils.Gap
+import com.jihan.composeutils.copyToClipboard
 
 @Composable
 fun HomeScreen(
-   navigate:(Routes)-> Unit,
+    navigate: (Routes) -> Unit,
 ) {
 
+    val context = LocalContext.current
+    var token by rememberSaveable { mutableStateOf("") }
 
-    Column {
+    var mToken by remember { mutableStateOf("") }
+    var mTitle by remember { mutableStateOf("") }
+    var mBody by remember { mutableStateOf("") }
 
-        var state by remember { mutableStateOf(false) }
-
-
-        Text(
-            stringResource(R.string.hello_android), fontSize = 35.sp
-        )
-
-        Spacer(Modifier.height(20.dp))
-
-        Button(
-            onClick = { navigate(Routes.LoginRoute) }
-        ) {
-            Text(stringResource(R.string.goToLoginScreen))
+    LaunchedEffect(true) {
+        if (token.isNotEmpty()) {
+            return@LaunchedEffect
         }
-
+        Firebase.messaging.token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                token = task.result
+            }
+        }
     }
 
+
+
+
+
+    Scaffold {
+        Column(Modifier
+            .fillMaxSize()
+            .padding(it)
+            .padding(16.dp)) {
+
+            Row {
+                TextField(
+                    value = token,
+                    singleLine = true,
+                    onValueChange = {},
+                    placeholder = { Text("My Token For This Device") },
+                    enabled = false,
+                    readOnly = true,
+                    label = { Text("My Token For This Device") },
+                    modifier = Modifier.weight(1f),
+                    trailingIcon = {
+                        IconButton(onClick = {
+                            token.copyToClipboard(context, token)
+                        }) {
+                            Icon(Lucide.Copy, null)
+                        }
+                    })
+            }
+            Gap(20)
+
+//            Text("Access Token: ${AccessToken().accessToken}")
+            Gap(10)
+
+            CxEditText(
+                value = mTitle, "Enter Title"
+            ) { mTitle = it }
+
+            Gap(10)
+            CxEditText(
+                value = mBody, "Enter Body"
+            ) { mBody = it }
+            Gap(10)
+            CxEditText(
+                value = mToken, "Enter Token"
+            ) { mToken = it }
+
+
+
+            CenterBox {
+                CxButton("Image Notification") {
+
+                    sendImageNotification(
+                        topic = "general",
+                        title = "Breaking News!",
+                        message = "Check out our latest article about technology trends",
+                        imageUrl = ""
+                    )
+                }
+            }
+        }
+    }
+
+
 }
+
+
